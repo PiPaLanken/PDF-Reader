ba47e49080f566ea4287300443efbd7e95ff825f
Sven Mayer <mayersven97@web.de>
2021-03-05 22:26:52 +0100

    +selldocumentreader added +taxchecker added

--- Classes/SellDoc.cs
+++ Classes/SellDoc.cs
@@ -6,13 +6,20 @@ namespace PDF_Reader.Classes
 {
     class SellDoc : Document
     {
+        public float CapitalTax = 0;
+        public float ChurchTax = 0;
+        public float SolidTax = 0;
+        public Document Doc;
         public SellDoc(string date,string name, float shares, float shareprice, float provision, float finalAmount, float capitalTax, float churchTax, float solidTax) : base(date, name, shares, shareprice, provision, finalAmount)
         {
             
         }
-        public SellDoc(Document bDoc, float CapitalTax, float ChurchTax, float solidTax) : base(bDoc.Date, bDoc.Name, bDoc.Shares, bDoc.SharePrice, bDoc.Provision, bDoc.FinalAmount)
+        public SellDoc(Document bDoc, float capitalTax, float churchTax, float solidTax) : base(bDoc.Date, bDoc.Name, bDoc.Shares, bDoc.SharePrice, bDoc.Provision, bDoc.FinalAmount)
         {
-
+            Doc = bDoc;
+            CapitalTax = capitalTax;
+            ChurchTax = churchTax;
+            SolidTax = solidTax;
         }
     }
 }
--- Process/ContentReader.cs
+++ Process/ContentReader.cs
@@ -49,21 +49,31 @@ namespace PDF_Reader.Process
         } 
         public static Document GetBasicInformationOutOfPath(string path)
         {
-            string date = GetValueOutOfPDFBetween("Datum:", "Sven", path);
-            string name = GetValueOutOfPDFBetween("Wertpapierbezeichnung","Nominale", path);
-            float shares =  StringToFloat(GetValueOutOfPDFBetween("Stück", "Kurs", path));
-            float sharePrice = StringToFloat(GetValueOutOfPDFBetween("Kurs", "Handelsplatz", path));
-            float provision = StringToFloat(GetValueOutOfPDFBetween("Provision", "Endbetrag", path));
-            float finalAmount = StringToFloat(GetValueOutOfPDFBetween("Lasten", "Abrechnungs-IBAN", path));
+            string date = GetValueOutOfPDFBetween("Datum:", "Sven", path,0);
+            string name = GetValueOutOfPDFBetween("Wertpapierbezeichnung","Nominale", path,0);
+            float shares =  StringToFloat(GetValueOutOfPDFBetween("Stück", "Kurs", path, 0));
+            float sharePrice = StringToFloat(GetValueOutOfPDFBetween("Kurs", "Handelsplatz", path, 0));
+            float provision = StringToFloat(GetValueOutOfPDFBetween("Provision", "Endbetrag", path, 0));
+            float finalAmount = StringToFloat(GetValueOutOfPDFBetween("Endbetrag", "Abrechnungs-IBAN", path, 0));
 
             return new Document(date,name,shares,sharePrice,provision,finalAmount);
         }
 
-        public static String GetValueOutOfPDFBetween(string name1, string name2,string path)
+        public static SellDoc GetSellInformationOutOfPath(string path)
+        {
+            Document document = GetBasicInformationOutOfPath(path);
+            float CapitalTax = StringToFloat(GetValueOutOfPDFBetween("Kapitalertragsteuer", "Kirchensteuer", path, 1));
+            float ChurchTax = StringToFloat(GetValueOutOfPDFBetween("Kirchensteuer", "Solidaritätszuschlag", path, 1));
+            float solidTax = StringToFloat(GetValueOutOfPDFBetween("Solidaritätszuschlag", "Provision", path, 1));
+
+            return new SellDoc(document,CapitalTax,ChurchTax,solidTax);
+        }
+
+        public static String GetValueOutOfPDFBetween(string name1, string name2,string path, int skipNumericWordsAfterBeforeValue)
         {
             PdfDocument pdfDocument = PdfDocument.Open(@path);
             List<string> wordsInDocument = GetWordsOutOfDocument(pdfDocument);
-            string result = FilterWordToVariable(wordsInDocument, name1, name2);
+            string result = FilterWordToVariable(wordsInDocument, name1, name2, skipNumericWordsAfterBeforeValue);
             return result;
         }
 
@@ -80,30 +90,46 @@ namespace PDF_Reader.Process
             return result;
         }
 
-        public static SellDoc GetSellInformationOutOfPath(string path)
-        {
-            return null;
-        }
-
-        public static string FilterWordToVariable(List<string> wordsInDocument, string beforeValue, string afterValue)
+        public static string FilterWordToVariable(List<string> wordsInDocument, string beforeValue, string afterValue, int skipNumericWordsAfterBeforeValue)
         {
             string result = "";
             List<string> nameBlocks = new List<string>();
             int min = wordsInDocument.IndexOf(beforeValue);
             int max = wordsInDocument.IndexOf(afterValue);
+            min += skipNumericWordsAfterBeforeValue;
             int diff = max - min;
             if (min > 0 && max > 0)
+            {
                 for (int x = 1; x < diff; x++)
                 {
                     int number = min + x;
                     nameBlocks.Add(wordsInDocument[number]);
                 }
-            foreach (string name in nameBlocks)
+                foreach (string name in nameBlocks)
+                {
+                    result += name + " ";
+                }
+            }
+            else if (!CheckIfTaxesAreIncluded(wordsInDocument))
             {
-                result += name + " ";
+                return "0";
             }
             return result;
         }
+
+        public static bool CheckIfTaxesAreIncluded(List<string> WordsInDocument)
+        {
+            List<string> Words = WordsInDocument;
+            foreach (string word in Words)
+            {
+                if (word.Contains("Kapitalertragsteuer"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public static float StringToFloat(string value)
         {
             string result = Regex.Replace(value, "[^0-9.,+-]", "");
